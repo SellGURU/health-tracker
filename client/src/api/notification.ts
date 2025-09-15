4b5b55b5bc56e5cbed5bc11713918e25af09dea2
amir <amirdehghani137722@gmail.com>
2025-09-15 12:34:51 +0330

    update endPoints

--- client/src/api/notification.ts
+++ client/src/api/notification.ts
@@ -4,7 +4,6 @@ class NotificationApi extends Api {
     static registerToken = (token:string) => {
         return this.post("/notif/token", {
             fcm_token: token,
-            patients_id:null
         });
     }
 }
