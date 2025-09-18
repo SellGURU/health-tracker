b6bea9739750b71921d9edf294c50a8c1f2b69a4
amir <amirdehghani137722@gmail.com>
2025-09-18 09:16:17 +0330

    update android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.innovatifyltd',
+  appId: "com.innovatifyltd",
   appName: 'Holosticare',
   webDir: 'dist/public',
   plugins: {
